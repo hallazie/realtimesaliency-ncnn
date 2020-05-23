ad3352df325d0373cb6b47d2e5d1a77947b2455e
肖尚华 <hallazie@gmail.com>
2020-05-24 03:15:17 +0800

    --amend

--- app/src/main/java/com/example/blankproject/MainActivity.java
+++ app/src/main/java/com/example/blankproject/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.blankproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Camera;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -14,45 +18,67 @@ import com.camerakit.CameraKitView;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
     private CameraKitView cameraKitView;
-//    private ImageView attentionView;
+    private ImageView attentionView;
     private Button switcher;
-
-    private View.OnClickListener photoOnClickListener = new View.OnClickListener(){
-        @Override
-        public void onClick(View v) {
-            System.out.println("click out...");
-            cameraKitView.captureImage(new CameraKitView.ImageCallback() {
-                @Override
-                public void onImage(CameraKitView cameraKitView, final byte[] capturedImage) {
-                    File savePhoto = new File(Environment.getExternalStorageDirectory(), "photo.jpg");
-                    System.out.println("click detected!!");
-                    try{
-                        FileOutputStream outputStream = new FileOutputStream(savePhoto);
-                        outputStream.write(capturedImage);
-                        outputStream.close();
-                    }catch (java.io.IOException e){
-                        e.printStackTrace();
-                    }
-                }
-            });
-        }
-    };
-
+    private Boolean showAttentionFlag = false;
+    private Integer globalCount = 0;
+    private Bitmap sampleSaliencyBitmap = BitmapFactory.decodeStream(getClass().getResourceAsStream("/res/drawable/saliency.jpeg"));
+//    private Bitmap sampleSaliencyBitmap = BitmapFactory.decodeStream(getClass().getResourceAsStream("/res/drawable/led.png"));
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cameraKitView = findViewById(R.id.camera);
-//        attentionView = findViewById(R.id.attention);
+        attentionView = findViewById(R.id.attention);
+        attentionView.setAlpha(0.5f);
         switcher = findViewById(R.id.switcher);
-        cameraKitView.setOnClickListener(photoOnClickListener);
+        switcher.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                showAttentionFlag = !showAttentionFlag;
+                if(showAttentionFlag){
+                    switcher.setText("停止检测");
+                    Bitmap newSample = setAlpha(sampleSaliencyBitmap);
+                    attentionView.setImageBitmap(newSample);
+                }else{
+                    switcher.setText("开始检测");
+                    attentionView.setImageDrawable(null);
+                }
+            }
+        });
     }
 
+    static Bitmap setAlpha(Bitmap bitmap){
+        int[] pixels = new int[bitmap.getWidth() * bitmap.getHeight()];
+        bitmap.getPixels(pixels, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        for (int i = 0; i < pixels.length; i++) {
+            int pixel = pixels[i];
+            int R = (pixel >> 16) & 0xff;
+            int newPixel = (R << 24) | R;
+            pixels[i] = newPixel;
+        }
+        bitmap = bitmap.copy( Bitmap.Config.ARGB_8888 , true);
+        bitmap.setPixels(pixels, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        return bitmap;
+    }
+
+    static Bitmap convert(Bitmap bitmap){
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        Bitmap newBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        for(int i=0; i<height; i++){
+            for(int j=0; j<width; j++){
+                newBitmap.setPixel(j, i, bitmap.getPixel(j, height-i-1));
+            }
+        }
+        return newBitmap;
+    }
 
     @Override
     protected void onStart() {
